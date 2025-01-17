adf1ce1dd764cf18da1887f8753b1f829dfc21d0
yushinc <cys74351@gmail.com>
2025-01-17 15:45:40 +0900

    초기 프로젝트 세팅

---
+++ src/main/java/com/example/GURU2_7team_Server/Guru27teamServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.GURU2_7team_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guru27teamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Guru27teamServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GURU2_7team_Server/Guru27teamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GURU2_7team_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Guru27teamServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
